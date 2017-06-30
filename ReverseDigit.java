d2f74cd5e8731c6116aba42fe296997a1966bb61
SATHISH15 <sathishsh1236@gmail.com>
2017-06-30 12:07:56 +0530

    Add files via upload

---
+++ ReverseDigit.java
@@ -0,0 +1,23 @@
+package Guvi;
+
+import java.util.Scanner;
+
+public class ReverseDigit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner s=new Scanner(System.in);
+int n=s.nextInt();
+int digit=(int)Math.log10(n)+1;
+int temp;
+int a[]=new int[digit];
+for(int i=0;i<digit;i++){
+	temp=n%10;
+	a[i]=temp;
+	n=n/10;
+}for(int i=0;i<digit;i++){
+System.out.print(a[i]);
+}s.close();
+	}
+
+}
