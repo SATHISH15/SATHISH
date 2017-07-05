b0bda49718ca0e87e6055c89c7b74be513a4afae
SATHISH15 <sathishsh1236@gmail.com>
2017-07-05 12:32:31 +0530

    Add files via upload

---
+++ Player_p4.java
@@ -0,0 +1,16 @@
+package Guvi;
+
+import java.util.Scanner;
+
+public class Player_p4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner s=new Scanner(System.in);
+		String str=s.nextLine();
+		int a=Integer.parseInt(str);
+		System.out.println(a);
+		s.close();
+	}
+
+}
