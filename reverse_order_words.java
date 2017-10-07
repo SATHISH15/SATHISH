e5bae720813a631de07e65c66fea41eb71cf06ee
SATHISH15 <sathishsh1236@gmail.com>
2017-10-07 13:34:55 +0530

    Add files via upload

---
+++ reverse_order_words.java
@@ -0,0 +1,21 @@
+package dsad;
+
+import java.util.*;
+
+public class reverse_order_words {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner ss=new Scanner(System.in);
+		String str=ss.nextLine();
+		String arr[]=str.split(" ");
+		for(int i=0;i<arr.length;i++) {
+			String s1=arr[i];
+			StringBuffer sb=new StringBuffer(arr[i]);
+			
+			String s2=sb.reverse().toString();
+			System.out.print(s2+" ");			
+		}
+	}
+
+}
