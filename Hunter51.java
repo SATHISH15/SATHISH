1128c0518974f3d65f30f4729a2808c7a91d3e33
SATHISH15 <sathishsh1236@gmail.com>
2017-10-07 15:22:58 +0530

    Add files via upload

---
+++ Hunter51.java
@@ -0,0 +1,19 @@
+package dsad;
+
+import java.util.*;
+
+public class Hunter51 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner ss=new Scanner(System.in);
+		String str=ss.next();
+		String str1=ss.next();
+		long a=(long)Integer.parseInt(str);
+		long b=(long)Integer.parseInt(str1);
+		long m=a*b;
+		String str3=String.valueOf(m);
+		System.out.println(str3);
+	}
+
+}
